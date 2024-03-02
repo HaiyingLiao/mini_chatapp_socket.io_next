bf8066f90513ec16f78dff4b926d4164c7226ec8
Hannah-Liao <hanna.liao@outlook.com>
2024-03-02 13:06:20 +0000

    update UI

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import io from 'socket.io-client';
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useRef } from 'react';
 
 const socket = io();
 
@@ -11,14 +11,20 @@ type MessageType = {
 };
 
 export default function Home() {
-	const [username, setUsername] = useState('');
-	const [chosenUsername, setChosenUsername] = useState('');
-	const [message, setMessage] = useState('');
+	const messagesEndRef = useRef<null | HTMLDivElement>(null);
+	const [username, setUsername] = useState<string>('');
+	const [startChat, setStartChat] = useState<boolean>(false);
+	const [message, setMessage] = useState<string>('');
 	const [messages, setMessages] = useState<Array<MessageType>>([]);
+
+	const sendMessage = async (e: React.FormEvent<HTMLFormElement>) => {
+		e.preventDefault();
+		socket.emit('chat message', { author: username, message });
+		setMessage('');
+	};
 
 	useEffect(() => {
 		const handleMessage = (msg: MessageType) => {
-			console.log('I am useEffect');
 			setMessages((prevMsgs) => {
 				return [...prevMsgs, { author: msg.author, message: msg.message }];
 			});
@@ -30,86 +36,76 @@ export default function Home() {
 		};
 	}, []);
 
-	const sendMessage = async () => {
-		socket.emit('chat message', { author: chosenUsername, message });
-		setMessage('');
-	};
-
-	const handleKeypress = (e: React.KeyboardEvent<HTMLInputElement>) => {
-		//it triggers by pressing the enter key
-		if (e.keyCode === 13) {
-			if (message) {
-				sendMessage();
-			}
-		}
-	};
+	useEffect(() => {
+		// This will scroll the messages container to the bottom whenever the messages array changes
+		messagesEndRef.current?.scrollIntoView({ behavior: 'smooth' });
+	}, [messages]);
 
 	return (
-		<div className='flex items-center p-4 mx-auto min-h-screen justify-center bg-purple-500'>
-			<main className='text-black gap-4 flex flex-col items-center justify-center w-full h-full '>
-				{!chosenUsername ? (
-					<>
-						<h3 className='font-bold text-white text-xl'>
-							How people should call you?
-						</h3>
+		<main className='w-full h-full text-black text-2xl'>
+			{!startChat ? (
+				<form
+					onSubmit={() => setStartChat(true)}
+					className='mx-auto my-[20%] w-96 flex flex-col items-center gap-3'
+				>
+					<h3 className='font-bold text-xl'>Please enter your username</h3>
+					<div className='flex gap-3'>
 						<input
 							type='text'
-							placeholder='Identity...'
+							placeholder='Username...'
 							value={username}
-							className='p-3 rounded-md outline-none text-black'
+							className='p-3 rounded-md outline-none'
 							onChange={(e) => setUsername(e.target.value)}
+							required
 						/>
-						<button
-							onClick={() => {
-								setChosenUsername(username);
-							}}
-							className='bg-white rounded-md px-4 py-2 text-xl text-black'
-						>
-							Go!
+						<button type='submit' className='bg-white rounded-md p-3 '>
+							Submit
 						</button>
-					</>
-				) : (
-					<>
-						<p className='font-bold text-white text-xl'>
-							Your username: {username}
-						</p>
-						<div className='flex flex-col justify-end bg-white h-[20rem] min-w-[33%] rounded-md shadow-md '>
-							<div className='h-full last:border-b-0 overflow-y-scroll'>
-								{messages.map((msg, i) => {
-									return (
-										<div
-											className='w-full py-1 px-2 border-b border-gray-200'
-											key={i}
-										>
-											{msg.author} : {msg.message}
-										</div>
-									);
-								})}
-							</div>
-							<div className='border-t border-gray-300 w-full flex rounded-bl-md'>
-								<input
-									type='text'
-									placeholder='New message...'
-									value={message}
-									className='outline-none py-2 px-2 rounded-bl-md flex-1'
-									onChange={(e) => setMessage(e.target.value)}
-									onKeyUp={handleKeypress}
-								/>
-								<div className='border-l border-gray-300 flex justify-center items-center  rounded-br-md group hover:bg-purple-500 transition-all'>
-									<button
-										className='group-hover:text-white px-3 h-full'
-										onClick={() => {
-											sendMessage();
-										}}
-									>
-										Send
-									</button>
-								</div>
-							</div>
-						</div>
-					</>
-				)}
-			</main>
-		</div>
+					</div>
+				</form>
+			) : (
+				<>
+					<ul className='h-full overflow-y-scroll mb-12 mx-1'>
+						{messages.map((msg, i) => {
+							return (
+								<li
+									className={`w-full flex ${
+										msg.author === username ? 'justify-start' : 'justify-end'
+									} m-1`}
+									key={i}
+								>
+									<div className='bg-white w-fit rounded py-1 px-2 '>
+										<span className='font-bold'>{msg.author} :</span>
+										{msg.message}
+									</div>
+								</li>
+							);
+						})}
+						<div ref={messagesEndRef} />
+					</ul>
+
+					<form
+						onSubmit={sendMessage}
+						className='bg-[#00000026] p-1 flex h-12 fixed bottom-0 left-0 w-full'
+					>
+						<input
+							type='text'
+							placeholder='New message...'
+							value={message}
+							className='outline-none px-4 rounded-tl-md rounded-br-md flex-1 m-1'
+							onChange={(e) => setMessage(e.target.value)}
+							required
+						/>
+
+						<button
+							type='submit'
+							className='text-white rounded-md px-4 m-1 bg-[#333] hover:opacity-90'
+						>
+							Send
+						</button>
+					</form>
+				</>
+			)}
+		</main>
 	);
 }
